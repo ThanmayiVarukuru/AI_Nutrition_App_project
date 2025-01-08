25255a7133a32d942f1c3b09b46d61e6fbdf4d15
ThanmayiVarukuru <91600952+ThanmayiVarukuru@users.noreply.github.com>
2025-01-08 20:30:43 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-### Health Management APP
+### Nutrition App Using Gemini Model
+
+#importing required libraries 
 from dotenv import load_dotenv 
 load_dotenv() #load all the ennv variables
 import streamlit as st
@@ -9,11 +11,13 @@ from PIL import Image
 #configure the genai library by providing API key
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+#implementing the function to get gemini response
 def get_gemini_response(input_text, image):
     model = genai.GenerativeModel('gemini-1.5-flash')
     response = model.generate_content([input_text, image[0]])
     return response.text
 
+#implementing a function to read the image and set the image format
 def input_image_setup(uploaded_file):
     #check if a file as uploaded or not 
     if uploaded_file is not None:
@@ -27,7 +31,8 @@ def input_image_setup(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
-#initialise the streamlit app
+
+#deploy the model using streamlit framework
 st.set_page_config(page_title="AI Nutritionist App")
 st.header("AI Nutritionist App")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -37,6 +42,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 submit = st.button("Tell me the calories")
 
+#writing a prompt for gemini model 
 input_prompt = """ 
     You are an expert in nutritionist where you need to see the food items from the image 
     and calculate the total calories, also provide the details of every food items with calories intake 
@@ -52,7 +58,6 @@ input_prompt = """
     and tell if the meal is Managing Diabetes or useful of body building or weight loss 
 """
 #if submit button is clicked 
-
 if submit:
     image_data = input_image_setup(uploaded_file)
     response = get_gemini_response(input_prompt, image_data)
